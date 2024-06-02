2b13af34e862ad07cc89a87ef42822ac1af97a1f
Garrett Hale <gthkeno@gmail.com>
2024-06-01 21:45:04 -0500

    meat atlased, tested will box

--- Props/PropBase.gd
+++ Props/PropBase.gd
@@ -17,11 +17,10 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	while i < state.get_contact_count():
 		var vec := state.get_contact_collider_velocity_at_position(i)
 		var force := vec.length_squared()
-		print(force)
 		if force > durability_sq:
 			pass # shatter
 			GameData.score -= cash_value * (5 if on_will else 1)
 			queue_free()
-		elif force > 10:
+		elif force > 64:
 			pass # todo clank sound
 		i += 1
