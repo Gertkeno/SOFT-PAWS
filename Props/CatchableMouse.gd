cb894f9eb466a3a48008edb3c01eab867ede816c
Garrett Hale <gthkeno@gmail.com>
2024-06-02 14:28:47 -0500

    shattering props, awaiting sounds

--- Props/CatchableMouse.gd
+++ Props/CatchableMouse.gd
@@ -1,12 +1,15 @@
 extends PathFollow3D
 
+@export var bonus_score: int = 1000
 @export var start_alert: bool = true
 @export_range(0, 5, 0.05) var speed: float = 0.5
+@onready var animation_tree: AnimationTree = $AnimationTree
 
 func _ready() -> void:
 	set_process(start_alert)
 	if start_alert:
 		$Prop/mouse2/AnimationPlayer.play("run cycle")
+	animation_tree.set("parameters/conditions/started_alert", start_alert)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,8 +18,14 @@ func _process(delta: float) -> void:
 
 func _on_prop_grabbed(pickable: Variant, by: Variant) -> void:
 	set_process(false)
-	# TODO: fucking explode
+	animation_tree.set("parameters/conditions/picked_up", true)
 
 func alert() -> void:
-	# TODO: Play alert animation
 	set_process(true)
+	animation_tree.set("parameters/conditions/alerted", true)
+
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "pickedup":
+		pass # TODO: particles and yipee!!
+		GameData.score += bonus_score
--- Props/PropBase.gd
+++ Props/PropBase.gd
@@ -4,12 +4,23 @@ class_name PropBase
 
 @export var cash_value: int = 10
 @export var on_will: bool = false
-@export_range(100,10_000) var durability: float = 1000
+@export_range(10, 1000, 0.5, "or_less") var durability: float = 15
 var durability_sq: float
+
+const PROP_BREAK_EFFECT = preload("res://Props/PropBreakEffect.tscn")
+@onready var clank_sound_player: AudioStreamPlayer3D = $ClankSoundPlayer
 
 func _ready() -> void:
 	super()
 	durability_sq = durability*durability
+
+
+func shatter() -> GPUParticles3D:
+	var clone := PROP_BREAK_EFFECT.instantiate()
+	clone.position = position
+	clone.emitting = true
+	add_sibling(clone)
+	return clone
 
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
@@ -17,12 +28,10 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	while i < state.get_contact_count():
 		var vec := state.get_contact_collider_velocity_at_position(i)
 		var force := vec.length_squared()
-		if force > 10:
-			print(name, "> ", force)
 		if force > durability_sq:
-			pass # shatter
 			GameData.score -= cash_value * (5 if on_will else 1)
-			queue_free()
+			shatter()
+			drop_and_free()
 		elif force > 64:
-			pass # todo clank sound
+			clank_sound_player.play()
 		i += 1
