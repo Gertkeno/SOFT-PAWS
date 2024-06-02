fe29bf22f16a28c47ab650044b74cb511e99df94
Garrett Hale <gthkeno@gmail.com>
2024-06-02 12:50:20 -0500

    xr player bigger pokers

--- Scenes/WillCardboardBox.gd
+++ Scenes/WillCardboardBox.gd
@@ -3,10 +3,11 @@ extends Area3D
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is PropBase:
+		# TODO: show monetary value increase
 		if body.on_will:
 			$SuccessParticle.restart()
 			GameData.score += body.cash_value
 		else:
 			$FailureParticle.restart()
 			GameData.score -= body.cash_value
-		body.queue_free()
+		body.drop_and_free()
